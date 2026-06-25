47859cdfc4558cb39dcbe92e1cacb4d5f1364675
Bernardo Lisboa <bernardo.al2006@aluno.ifsc.edu.br>
2026-06-25 19:27:32 -0300

    fix: build gradle funcionando

--- src/main/java/ads/lll/academia/SistemaAcademiaApplication.java
+++ src/main/java/ads/lll/academia/SistemaAcademiaApplication.java
@@ -4,8 +4,10 @@ import ads.lll.academia.views.Menu;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("!test")
 public class SistemaAcademiaApplication implements CommandLineRunner {
 
 	private final Menu menu;
--- src/test/java/ads/lll/academia/AcademiaApplicationTests.java
+++ src/test/java/ads/lll/academia/AcademiaApplicationTests.java
@@ -3,8 +3,10 @@ package ads.lll.academia;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AcademiaApplicationTests {
 
 	@Test
